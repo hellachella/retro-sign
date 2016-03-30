9efb2ece4420a98bec9e3a7f2bec276e150857d0
Michelle <vand0432@algonquinlive.com>
2016-03-29 21:29:31 -0400

    So many children?

--- js/main.js
+++ js/main.js
@@ -6,13 +6,20 @@ var $engage = $('.engage');
 $btn.on('click', function () {
   $lines.addClass('linify');
   $lines.removeClass('lines');
+  $engage.children().css('fill', '#f60');
+
+  $engage.children('path:nth-child(1)').css('transition', 'all .5s');
+  $engage.children('path:nth-child(6)').css('transition', 'all .5s');
+  $engage.children('path:nth-child(2)').css('transition', 'all 1s');
+  $engage.children('path:nth-child(5)').css('transition', 'all 1s');
+  $engage.children('path:nth-child(3)').css('transition', 'all 1.5s');
+  $engage.children('path:nth-child(4)').css('transition', 'all 1.5s');
+  $engage.children().css('transition-delay', '.5s');
 });
 
-$btn.on('mouseenter', function (){
-$('svg btn:nth-child(2)').css('fill', '#f60');
-
+$btn.on('mouseenter', function () {
+  $(this).children('path:last-child').css('fill', '#f60');
 });
-
 
 
 /*
