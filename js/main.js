2831adac333c7bba58932ea20c134d8915aeb741
Michelle <vand0432@algonquinlive.com>
2016-03-29 20:40:53 -0400

    nope.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 var $svg = $('svg');
 var $lines = $('.lines');
-var $btn = $('.btn')
+var $btn = $('.btn');
+var $engage = $('.engage');
 
 $btn.on('click', function () {
   $lines.addClass('linify');
   $lines.removeClass('lines');
 });
+
+$btn.on('mouseenter', function (){
+$('svg btn:nth-child(2)').css('fill', '#f60');
+
+});
+
+
 
 /*
 $('.btn').on('click', function () {
