fc546d2a90971bc6c733b0cd2b51c46e17caa08e
Michelle <vand0432@algonquinlive.com>
2016-03-31 17:09:19 -0400

    Changed massive amount of js to css.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,20 @@ var $lines = $('.lines');
 var $btn = $('.btn');
 var $engage = $('.engage');
 
+
+
+$btn.on('click', function() {
+$lines.addClass('linify');
+$lines.removeClass('lines');
+$engage.addClass('is-engaged');
+});
+
+$btn.on('mouseenter', function () {
+  $(this).children('path:last-child').css('fill', '#f60');
+});
+
+
+/*
 $btn.on('click', function () {
   $lines.addClass('linify');
   $lines.removeClass('lines');
@@ -15,11 +29,9 @@ $btn.on('click', function () {
   $engage.children('path:nth-child(3)').css('transition', 'all 1.5s');
   $engage.children('path:nth-child(4)').css('transition', 'all 1.5s');
   $engage.children().css('transition-delay', '.5s');
-});
+});*/
 
-$btn.on('mouseenter', function () {
-  $(this).children('path:last-child').css('fill', '#f60');
-});
+
 
 
 /*
