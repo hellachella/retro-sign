bca1a5cb633207dd5de82d10b958a67111127298
Michelle <vand0432@algonquinlive.com>
2016-04-02 23:03:31 -0400

    Fixed button (removed js)

--- js/main.js
+++ js/main.js
@@ -3,16 +3,17 @@ var $lines = $('.lines');
 var $btn = $('.btn');
 var $engage = $('.engage');
 
+/*
 $btn.on('mouseenter', function () {
   $(this).children('path:last-child').css('fill', '#f60');
 });
+*/
 
 $btn.on('click', function () {
   $lines.addClass('linify');
   $lines.removeClass('lines');
   $engage.addClass('is-engaged');
 });
-
 
 
 /*
